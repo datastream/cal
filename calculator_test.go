723184051813b04fd28987da77202923f2fc263d
datastream <guxianjie@gmail.com>
2012-10-11 21:20:10 +0800

    fix type

--- calculator_test.go
+++ calculator_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCal(t *testing.T) {
 	exp := "a/b-(c+d-a)*c+(b*c)*(a*c-c)"
-	k_v := map[string]float{"a":2,"b":3,"c":4,"d":5}
+	k_v := map[string]float32{"a":2,"b":3,"c":4,"d":5}
 	rst, err := cal.Cal(exp, k_v)
 	if err != nil {
 		t.Fatal(err, rst)
